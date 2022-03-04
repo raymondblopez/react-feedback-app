7318241673ee9a4e52a2abdc1c7358e56531a6e9
Raymond Lopez <raymondblopez97@gmail.com>
2022-03-03 20:16:57 -0500

    display feedback items

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import Header from './components/Header';
-import FeedbackItem from './components/FeedbackItem';
+import FeedbackList from './components/FeedbackList';
 import FeedbackData from './data/FeedbackData';
 import { useState } from 'react';
 
@@ -9,7 +9,7 @@ function App() {
     <>
       <Header />
       <div className='container'>
-        <FeedbackItem />
+        <FeedbackList feedback={feedback} />
       </div>
     </>
   );
