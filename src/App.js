34bee32989d15b885eefa2e663f3d8a2f51d28d6
Raymond Lopez <raymondblopez97@gmail.com>
2022-03-04 00:29:55 -0500

    add delete handling

--- src/App.js
+++ src/App.js
@@ -5,11 +5,16 @@ import { useState } from 'react';
 
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData);
+  const deleteFeedback = (id) => {
+    if (window.confirm('Are you sure you want to delete this item?')) {
+      setFeedback(feedback.filter((item) => item.id !== id));
+    }
+  };
   return (
     <>
       <Header />
       <div className='container'>
-        <FeedbackList feedback={feedback} />
+        <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
       </div>
     </>
   );
--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -2,14 +2,16 @@ import React from 'react';
 import FeedbackItem from './FeedbackItem';
 import PropTypes from 'prop-types';
 
-function FeedbackList({ feedback }) {
+function FeedbackList({ feedback, handleDelete }) {
   if (!feedback || feedback.length === 0) {
     return <p>No Feedback Yet</p>;
   }
   return (
     <div className='feedback-list'>
       {feedback.map((item) => {
-        return <FeedbackItem key={item.id} item={item} />;
+        return (
+          <FeedbackItem key={item.id} item={item} handleDelete={handleDelete} />
+        );
       })}
     </div>
   );
